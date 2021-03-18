f4a6eb2a83964002c91ddfd0eb42e22758f3cdb1
Luis Mercado <luisbzmercado1776@gmail.com>
2021-03-18 14:29:15 +0800

    fix bug in technologies not initializing - change project img to array of imgs

--- src/app/models/project-display/project-display.ts
+++ src/app/models/project-display/project-display.ts
@@ -1,13 +1,13 @@
 export class ProjectDisplay {
   projectName: string;
-  projectImgUrl: string;
+  projectImgUrls: string[];
   projectDescription: string;
   technologies: string[];
 
-  constructor(projectName: string, projectImageUrl: string, projectDescription: string, technologies: string[]){
+  constructor(projectName: string, projectImageUrls: string[], projectDescription: string, technologies: string[]){
     this.projectName = projectName;
-    this.projectImgUrl = projectImageUrl;
+    this.projectImgUrls = projectImageUrls;
     this.projectDescription = projectDescription;
-    this.technologies = [];
+    this.technologies = technologies;
   }
 }
