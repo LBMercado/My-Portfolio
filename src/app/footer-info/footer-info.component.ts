9b165fbe99be3752e52001681d3062a4face2a7a
Luis Mercado <luisbzmercado1776@gmail.com>
2021-03-26 11:22:37 +0800

    inject portfolioOwnerService to footer info for name

--- src/app/footer-info/footer-info.component.ts
+++ src/app/footer-info/footer-info.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthorInfo } from '../models/author-info/author-info';
 import { CreditAuthorService } from '../services/credit-author.service';
+import { PortfolioOwnerService } from '../services/portfolio-owner.service';
 
 @Component({
   selector: 'app-footer-info',
@@ -11,13 +12,14 @@ export class FooterInfoComponent implements OnInit {
   authorReferences: AuthorInfo[];
   footerText: string;
 
-  constructor(private creditAuthorSvc: CreditAuthorService) {
-    this.footerText = 'This is a sample footer.';
+  constructor(private creditAuthorSvc: CreditAuthorService, private portfolioOwnerService: PortfolioOwnerService) {
+    this.footerText = 'Footer Text';
     this.authorReferences = [];
   }
 
   ngOnInit(): void {
     this.authorReferences = this.creditAuthorSvc.getAuthors();
+    this.footerText = `Dynamic portfolio project by ${this.portfolioOwnerService.getName()}`;
   }
 
 }
