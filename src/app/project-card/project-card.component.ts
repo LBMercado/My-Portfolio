9dd86f009e3524f51f33135d5f6479c8aaee7286
Luis Mercado <luisbzmercado1776@gmail.com>
2021-03-31 11:52:08 +0800

    add carousel element for project images - make initial styles to project card

--- src/app/project-card/project-card.component.ts
+++ src/app/project-card/project-card.component.ts
@@ -1,23 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 import { ProjectDisplay } from '../models/project-display/project-display';
-import { ProjectsService } from '../services/projects.service'
+import { ProjectsService } from '../services/projects.service';
 
 @Component({
   selector: 'app-project-card',
   templateUrl: './project-card.component.html',
-  styleUrls: ['./project-card.component.css']
+  styleUrls: ['./project-card.component.css'],
+  providers: [NgbCarouselConfig]
 })
 export class ProjectCardComponent implements OnInit {
   projectDisplays: ProjectDisplay[];
-  imgIndex: number;
-
-  constructor(private projSvc: ProjectsService) {
+  constructor(private projSvc: ProjectsService, carouselConf: NgbCarouselConfig) {
     this.projectDisplays = [];
-    this.imgIndex = 0;
+    carouselConf.interval = 5000;
+    carouselConf.wrap = true;
+    carouselConf.pauseOnHover = false;
   }
 
   ngOnInit(): void {
     this.projectDisplays = this.projSvc.getProjects();
   }
-
 }
